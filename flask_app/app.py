1e2deaf6ca927a6887c5afd3443934411895e78c
Evgeniy <evgen19-97@ya.ru>
2019-08-14 14:28:36 +0300

    add prediction

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, render_template, redirect
-
+from joblib import load
 app = Flask(__name__)
 
-def loadMOdel():
-    pass
+def loadMOdel(path='model/model.joblib'):
+    model = load(path)
+    return model
+
+model = loadMOdel()
 
 @app.route('/', methods=["GET"])
 def hi():
-    pass
+    return render_template('main.html')
 
 @app.route('/predict', methods=["POST"])
 def predict():
-    pass
+    text = request.text
+    predictionn = model.predict(text)
+    return predictionn
 
 
 @app.route('/add', methods=["POST"])
